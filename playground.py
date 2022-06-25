e6c310e6e66d2ca117874a7c2e4f872ce716d8b1
John Pine <75548572+jwpine@users.noreply.github.com>
2022-06-24 21:26:14 -0500

    Add files via upload

--- playground.py
+++ playground.py
@@ -8,5 +8,17 @@ year = 2022
 # Get the Complete Event List
 event_list = fastf1.get_event_schedule(year)
 
-#print(event_list)
-#event_list.to_excel("C:/Users/johnp/Downloads/events.xlsx")
+
+# send event list to csv
+event_list.to_csv("C:/Users/johnp/Downloads/events.csv")
+
+# Read csv to new dataframe because Fastf1 is a bit dumb
+event_list_df = pd.read_csv("C:/Users/johnp/Downloads/events.csv")
+
+# Repeat lines 5 times to get each session as a new line
+mid_df = event_list_df.loc[event_list_df.index.repeat(5)].reset_index(drop=True)
+mid_df.to_excel("C:/Users/johnp/Downloads/events_DF.xlsx")
+
+# Create a loop that drops columns
+
+
