cff78ddd3a81ce4f9c270e7fd759945c8536b597
John Pine <75548572+jwpine@users.noreply.github.com>
2022-07-04 17:21:32 -0500

    Add files via upload

--- Season_Data_Grabber.py
+++ Season_Data_Grabber.py
@@ -3,33 +3,31 @@ import pandas as pd
 import fastf1.core
 
 fastf1.Cache.enable_cache('cache_spot')
-# DataFrame to put all results
-laps_df = []
 
-# Read the list of events and grab the right columns to get every session
-event_list_df = pd.read_csv("C:/Users/johnp/Downloads/Event_Rename.csv")
-year = event_list_df["Year"].tolist()
-grand_prix = event_list_df["EventName"].tolist()
-day = event_list_df["Session Rename"].tolist()
+# Create Session LIst
+event_list_import = pd.read_csv("C:/Users/johnp/Downloads/Event_Rename.csv")
+year = event_list_import["Year"].tolist()
+grand_prix = event_list_import["EventName"].tolist()
+grand_prix_day = event_list_import["Session Rename"].tolist()
 
-# Create a starter to iterate through each event
+# Create a starter index for interating through the data
 index = 0
+com_df = pd.DataFrame()
 
-# Go through each session and create the session characteristics
+# Go through each session and get the data
 for event in grand_prix:
-    session = fastf1.get_session(year[index], grand_prix[index], day[index])
+    session = fastf1.get_session(year[index], grand_prix[index], grand_prix_day[index])
     session.load()
-    drivers = pd.unique(session.laps['Driver'])
-    for i in drivers:
-        i_session = session.laps.pick_driver(i)
-        laps_df.append(i_session)
-    results = pd.concat(laps_df)
+    laps = session.laps
+    weather_data = session.laps.get_weather_data()
+    laps = laps.reset_index(drop=True)
+    weather_data = weather_data.reset_index(drop=True)
+    event_list = {'Year': [year[index]], 'Grand_Prix': [grand_prix[index]], 'Event_List': [grand_prix_day[index]]}
+    event_list_df = pd.DataFrame(event_list)
+    event_list_df = event_list_df.loc[event_list_df.index.repeat(len(laps))].reset_index(drop=True)
+    joined_df = pd.concat([laps, weather_data.loc[:, ~(weather_data.columns == 'Time')], event_list_df], axis=1)
+    com_df = com_df.append(joined_df)
     index = index + 1
 
-# Print the results to an xlsx
-results.to_excel("C:/Users/johnp/Downloads/combined_laps.xlsx")
-
-
-
-
-
+# Print the results to an excel file
+com_df.to_csv("C:/Users/johnp/Downloads/season22.csv")
--- Session_Data_Grabber.py
+++ Session_Data_Grabber.py
@@ -5,23 +5,26 @@ import fastf1.core
 fastf1.Cache.enable_cache('cache_spot')
 
 year = 2022
-grand_prix = 'Canadian Grand Prix'
-grand_prix_day = 'Practice 1'
+grand_prix = 'British Grand Prix'
+grand_prix_day = 'Practice 2'
 
 # Create the Session
 session = fastf1.get_session(year, grand_prix, grand_prix_day)
 session.load()
 
-# Get the list of unique drivers in the session
-drivers = pd.unique(session.laps['Driver'])
+# Get the laps and weather
+laps = session.laps
+weather_data = session.laps.get_weather_data()
+laps = laps.reset_index(drop=True)
+weather_data = weather_data.reset_index(drop=True)
+event_list = {'Year': [year], 'Grand_Prix': [grand_prix], 'Event_List': [grand_prix_day]}
+event_list_df = pd.DataFrame(event_list)
+event_list_df = event_list_df.loc[event_list_df.index.repeat(len(laps))].reset_index(drop=True)
 
-# Iterate through drivers to get all their lap data
-df = []
-for i in drivers:
-    i_session = session.laps.pick_driver(i)
-    df.append(i_session)
-results = pd.concat(df)
+# exclude the 'time' column from weather and join the weather, laps, and event name together
+joined_data = pd.concat([laps, weather_data.loc[:, ~(weather_data.columns == 'Time')], event_list_df], axis=1)
 
 # Print the results to an excel file
-results.to_excel("C:/Users/johnp/Downloads/complete_session.xlsx")
+joined_data.to_excel("C:/Users/johnp/Downloads/playground2.xlsx")
+
 
