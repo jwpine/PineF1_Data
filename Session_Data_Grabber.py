96e672224e628b83ee1af1a202bee21b8ea2f096
John Pine <75548572+jwpine@users.noreply.github.com>
2022-07-24 20:55:00 -0400

    Update Session_Data_Grabber.py

--- Session_Data_Grabber.py
+++ Session_Data_Grabber.py
@@ -6,7 +6,8 @@ fastf1.Cache.enable_cache('cache_spot')
 
 year = 2022
 grand_prix = 'British Grand Prix'
-grand_prix_day = 'Practice 2'
+grand_prix_day = 'Qualifying'
+event_id = "X"
 
 # Create the Session
 session = fastf1.get_session(year, grand_prix, grand_prix_day)
@@ -17,7 +18,7 @@ laps = session.laps
 weather_data = session.laps.get_weather_data()
 laps = laps.reset_index(drop=True)
 weather_data = weather_data.reset_index(drop=True)
-event_list = {'Year': [year], 'Grand_Prix': [grand_prix], 'Event_List': [grand_prix_day]}
+event_list = {'Year': [year], 'Grand_Prix': [grand_prix], 'Event_List': [grand_prix_day], 'Event_ID': [event_id]}
 event_list_df = pd.DataFrame(event_list)
 event_list_df = event_list_df.loc[event_list_df.index.repeat(len(laps))].reset_index(drop=True)
 
@@ -25,6 +26,6 @@ event_list_df = event_list_df.loc[event_list_df.index.repeat(len(laps))].reset_i
 joined_data = pd.concat([laps, weather_data.loc[:, ~(weather_data.columns == 'Time')], event_list_df], axis=1)
 
 # Print the results to an excel file
-joined_data.to_excel("C:/Users/johnp/Downloads/playground2.xlsx")
+joined_data.to_excel("C:/Users/johnp/Downloads/SessionData.xlsx")
 
 
